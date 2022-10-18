9f72c332cf1a82366d4fd6b789685eb1b7eb6b3d
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-10-19 01:02:01 +0300

    Add ErrorWrapper tests.

---
+++ test/ErrorWrapperTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+use KGun\Errorise\{ErrorWrapper, ErrorException};
+
+class ErrorWrapperTest extends PHPUnit\Framework\TestCase
+{
+    function testWrap() {
+        $ret = ErrorWrapper::wrap(function () {
+            $fp = fopen('absent-file', 'r');
+            $fp && fclose($fp);
+            return !!$fp;
+        }, $e1);
+
+        $this->assertFalse($ret);
+
+        $this->assertInstanceOf(ErrorException::class, $e1);
+        $this->assertInstanceOf(\ErrorException::class, $e1);
+
+        $this->assertSame('fopen(absent-file): Failed to open stream: No such file or directory',
+            $e1->getMessage());
+        $this->assertSame(__FILE__, $e1->getFile());
+        $this->assertSame(10, $e1->getLine());
+        $this->assertSame('fopen', $e1->error()->getFunction());
+
+        $ret = ErrorWrapper::wrap(function () {
+            $fp = fopen(__FILE__, 'r');
+            $fp && fclose($fp);
+            return !!$fp;
+        }, $e2);
+
+        $this->assertTrue($ret);
+        $this->assertNull($e2);
+    }
+}
