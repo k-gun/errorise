65e1dc84bcdea1389ca29214946d29521e12d2d9
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-10-19 01:12:56 +0300

    Update bootstrap file.

--- test/boot.php
+++ test/boot.php
@@ -1,10 +1,13 @@
 <?php
+// Manual Test:
+// ~/.composer/vendor/bin/phpunit --bootstrap=./boot.php ErrorTest.php
+// ~/.composer/vendor/bin/phpunit --bootstrap=./boot.php ErrorTest.php --colors --testdox
 namespace KGun\Errorise;
 
 // Register autoload.
 spl_autoload_register(function ($name) {
     static $prefix = __NAMESPACE__ . '\\';
-    if (stripos($name, $prefix) === 0) {
+    if (strpos($name, $prefix) === 0) {
         $name = strtr(substr($name, strlen($prefix)), '\\', '/');
         $file = realpath(__DIR__ . '/../src/' . $name . '.php');
         $file && require $file;
