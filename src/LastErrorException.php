6ea60828c7a3b4affe693e717b7b88c91b55b402
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-10-22 23:19:58 +0300

    Add LastErrorException class.

---
+++ src/LastErrorException.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright (c) 2022 · Kerem Güneş
+ * Apache License 2.0 · https://github.com/k-gun/errorise
+ */
+declare(strict_types=1);
+
+namespace KGun\Errorise;
+
+/**
+ * Exception class for last occured errors.
+ *
+ * @package KGun\Errorise
+ * @object  KGun\Errorise\LastErrorException
+ * @author  Kerem Güneş
+ */
+class LastErrorException extends ErrorException
+{
+    /**
+     * Defaults for no errors / invalid calls.
+     *
+     * @var array
+     */
+    private static array $lastErrorDefault = [
+        'type' => 0,    'message' => 'No error',
+        'file' => null, 'line'    => null,
+    ];
+
+    /**
+     * Constructor.
+     *
+     * @param string         $message
+     * @param int            $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $message = '', int $code = 0, \Throwable $previous = null)
+    {
+        $severity = 0;
+        $file = $line = null;
+
+        if (!func_num_args()) {
+            // Merge with defaults.
+            $lastError = array_merge(self::$lastErrorDefault, (array) error_get_last());
+
+            // Re-assign variables extracting from last error.
+            ['type' => $severity, 'file' => $file, 'line' => $line, 'message' => $message] = $lastError;
+
+            $error = new Error(compact('severity', 'message', 'file', 'line'));
+        }
+
+        parent::__construct($message, $code, $severity, $file, $line, $previous, $error ?? null);
+    }
+}
