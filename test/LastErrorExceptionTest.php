77833cf5c8458ba453a5834f8769331192d76826
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-10-22 23:20:25 +0300

    Add LastErrorException test.

---
+++ test/LastErrorExceptionTest.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+use KGun\Errorise\{LastErrorException, ErrorException};
+
+class LastErrorExceptionTest extends PHPUnit\Framework\TestCase
+{
+    function testAll() {
+        $ex = new LastErrorException();
+
+        $this->assertSame(0, $ex->getSeverity());
+        $this->assertSame('No error', $ex->getMessage());
+        $this->assertSame('No error', $ex->getPureMessage());
+        $this->assertNull($ex->error()->getFunction());
+
+        // Required for "@", because of PHPUnit.
+        set_error_handler(fn() => false);
+
+        @mkdir('');
+        $ex = new LastErrorException();
+
+        restore_error_handler();
+
+        $this->assertSame(2, $ex->getSeverity()); // E_WARNING
+        $this->assertSame('mkdir(): No such file or directory', $ex->getMessage());
+        $this->assertSame('No such file or directory', $ex->getPureMessage());
+        $this->assertSame('mkdir', $ex->error()->getFunction());
+
+        $this->assertInstanceOf(ErrorException::class, $ex);
+        $this->assertInstanceOf(\ErrorException::class, $ex);
+    }
+}
